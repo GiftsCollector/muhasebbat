bfe592e3d0550612214a369fff3cc80f317c3d7a
GiftsCollector <ahmedfathyabdel.m@gmail.com>
2026-09-21 19:14:57 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build script for Render deployment
-
-echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Build completed successfully!"
